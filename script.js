ba94c31289a21003b3b6f3bca43db929df073c78
Ahmad <ahmad@test.example>
2024-01-27 15:10:39 -0330

    Add book deleting functionality
    Add reading toggling

--- script.js
+++ script.js
@@ -1,20 +1,73 @@
 function doAllTheWork () {
-    const myLibrary = [];
+    const myLibrary = [{number:1, name:"Jaraed", author:"Mammad", year: 1984, pages: 134, isRead: true}];
     function addBookToLibrary(bookName, bookAuthor, bookYear, bookPages, bookIsRead) {
         // const bookName = prompt("What's the book's name?");
         // const bookAuthor = prompt("Who's the book's author?");
         // const bookYear = prompt("In what year was the book published?");
-        const newBookObject = new Book(bookName,bookAuthor,bookYear, bookPages, bookIsRead);
+        const newBookObject = new Book(0, bookName,bookAuthor,bookYear, bookPages, bookIsRead);
         myLibrary.push(newBookObject);
-
-        function Book(name,author,year,pages,isRead) {
+        updateTable();
+        // function addToTable () {
+        //     const table = document.querySelector(".table");
+        //     const row = table.insertRow(-1);
+        //     const rowNumber = table.querySelector("tbody").childElementCount;
+        //     const rowNumberCell = row.insertCell(0);
+        //     rowNumberCell.innerHTML = rowNumber;
+        //     //Put data information in cells in rows
+        //     [bookName, bookAuthor, bookYear, bookPages, bookIsRead].forEach ( (tableField, index) => {
+        //         let cell = row.insertCell(index + 1);
+        //         cell.innerHTML = tableField;
+        //     } )
+        //     const deletCell = row.insertCell(-1);
+        //     deletCell.innerHTML = '<button class="delete">Delete</button>';
+        //     addRemoveBooksFunction();
+        // }
+        function Book(number, name,author,year,pages,isRead) {
+            this.number = number;
             this.name = name;
             this.author = author;
             this.year = year;
             this.pages = pages;
             this.isRead = isRead;
         }
-    addBookToLibrary(bookName, bookAuthor, bookYear, bookPages, bookIsRead);
+    }
+    function updateTable () {
+        const table = document.querySelector(".table");
+        const tbody = table.querySelector("tbody");
+        tbody.innerHTML='';
+        myLibrary.forEach ( (bookRowObject , index) => {
+            let row = tbody.insertRow(-1);
+            bookRowObject.number = index+1;
+
+            [bookRowObject.number, bookRowObject.name, bookRowObject.author ,
+                bookRowObject.year, bookRowObject.pages].forEach ( (field, index) => {
+                    let cell = row.insertCell(index);
+                    cell.innerHTML = field;
+                } )
+            let cell = row.insertCell(-1);
+            cell.innerHTML = '<input type="checkbox">';
+
+            const deletCell = row.insertCell(-1);
+            deletCell.innerHTML = '<button class="delete">Delete</button>';
+            console.log("update table function");
+        })
+        addRemoveToggleBooksFunction();
+    }
+    function addRemoveToggleBooksFunction () {
+        const deleteButtons = document.querySelectorAll(".table tbody button[class='delete']");
+        const isReadCheckButtons = document.querySelectorAll(".table tbody input[type='checkbox']");
+        deleteButtons.forEach ( (button) => {
+            button.addEventListener ("click", (event) => {
+                const rowNumber = event.target.parentNode.parentNode.rowIndex;
+                console.log(rowNumber);
+                myLibrary.splice(rowNumber-1, 1);
+                updateTable();
+                console.log(myLibrary);
+                myLibrary.forEach ( (book, index) => {
+                    book.number = index+1;
+                } )
+        })
+        })
     }
     function checkForm () {
         const form = document.querySelector(".form");
@@ -30,7 +83,7 @@ function doAllTheWork () {
             const errorSection = input.nextElementSibling;
             const type = input.getAttribute("name");
             input.addEventListener("input" , (event) => {
-                if (!input.validity.valid) {
+                if (type !== "read-or-not" && !input.validity.valid) {
                     showError(input, type);
                     event.preventDefault();
                 }
@@ -40,18 +93,30 @@ function doAllTheWork () {
                 }
             });
         })
-        //Check on form submission validity and all the input elements
+        //Add books to the library array and also
+        //check on form submission validity and all the input elements
         form.addEventListener ("submit" , (event) => {
+            let isThereError = false;
             [bookName, authorName, year, pages, readOrNot].forEach ((input) => {
                 const type = input.getAttribute("name");
-                if (!input.validity.valid) {
+                if (type !== "read-or-not" && !input.validity.valid) {
+                    isThereError = true;
                     showError(input, type);
+                   
                     event.preventDefault();
                 }
-                else {
+            });
+            if (!isThereError) {
+                addBookToLibrary(bookName.value,
+                authorName.value, year.value,
+                pages.value, readOrNot.checked);
 
-                }
-            })
+                console.log(myLibrary);
+                [bookName, authorName, year, pages, readOrNot].forEach ((input) => {
+                    input.value="";
+                });
+                event.preventDefault();
+            }
         })
 
         function showError (field , type) {
@@ -64,5 +129,7 @@ function doAllTheWork () {
             }
             errorSection.className = "error active";
         }
-    } checkForm();
-}
+    }
+    addRemoveToggleBooksFunction();
+    checkForm();
+}doAllTheWork();
